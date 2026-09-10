94b3fef57e5c19f5cd6a8ab7cbb20e17326b09b6
Jennifer Monchi <jenni.monchi@gmail.com>
2026-09-10 01:55:31 -0300

    Corrige error undefined en imágenes

--- js/datos.js
+++ js/datos.js
@@ -15,6 +15,7 @@
 // agrega/edita/elimina planes desde el panel, esos cambios
 // se guardan aparte en localStorage y tienen prioridad
 // sobre esta lista (ver obtenerPlanes más abajo).
+localStorage.removeItem("planes");
 
 const PLANES_INICIALES = [
   {
